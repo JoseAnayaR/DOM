8bdf11b155717a358bbab5c66ec7123745c3f785
José Anaya <joseanayautj@gmail.com>
2023-09-06 21:05:14 -0600

    Clase 05 completa

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-(() => {
+  import checkComplete from "./Componentes/checkComplete.js";
+  import deleteIcon from "./Componentes/deleteIcon.js";
   const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
@@ -9,42 +10,17 @@ const createTask = (evento) => {
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = "";
-    //Backticks
+
     const taskContent = document.createElement('div');
     taskContent.appendChild(checkComplete());
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerText = value;
     taskContent.appendChild(titleTask);
-    const content = `
-      <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  //task.innerHTML = content;
+    
   task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
   list.appendChild(task);
 
 };
-
-
-//Arrow function o funciones anonimas
-
-//listener
 btn.addEventListener("click", createTask );
-
-const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far", "fa-check-square", "icon");
-  i.addEventListener('click', completeTask);
-  return i
-};
-
-//Immediately invoked function expression IIFE
-
-const completeTask = (event) => {
-  const element = event.target;
-  element.classList.toggle('fas');
-  element.classList.toggle('completeIcon');
-  element.classList.toggle('far');
-};
-
-})();
-
